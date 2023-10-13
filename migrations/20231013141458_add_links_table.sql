933e208a8abb4a1a3857eec4eb5f7357ca43cc59
calinanca99 <calinanca99@gmail.com>
2023-10-13 16:23:39 +0200

    add migration to create links table

---
+++ migrations/20231013141458_add_links_table.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+create table links (
+    id uuid primary key,
+    user_id uuid references users(id) not null,
+    link text not null,
+    link_name text,
+    inserted_at timestamptz default now() not null,
+    updated_at timestamptz,
+    unique(user_id, link)
+)

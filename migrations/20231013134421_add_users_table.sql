c4e8a8b79658f336c77a07578c68bd4e6a633671
calinanca99 <calinanca99@gmail.com>
2023-10-13 16:14:03 +0200

    add migration for to create users table

---
+++ migrations/20231013134421_add_users_table.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+create table users (
+    id uuid primary key,
+    username varchar(255) not null unique,
+    password_hash text not null,
+    inserted_at timestamptz default now() not null,
+    updated_at timestamptz
+);
